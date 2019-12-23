f31d1a478d11532dd35bfef969c8cbf8fab1477c
Carl Scott <team.scott.solutions@gmail.com>
2019-12-23 11:19:36 -0500

    Simple ScrollView in settings that breaks

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import { ExpoConfigView } from '@expo/samples';
+import { ScrollView, Text, TouchableOpacity } from 'react-native';
 
-export default function SettingsScreen() {
-  /**
-   * Go ahead and delete ExpoConfigView and replace it with your content;
-   * we just wanted to give you a quick view of your config.
-   */
-  return <ExpoConfigView />;
+export default function SettingsScreen({ navigation: { push }}) {
+  return (
+    <ScrollView style={{ flex: 1 }}>
+      <TouchableOpacity onPress={() => { push('Settings'); }}>
+        <Text>Link</Text>
+      </TouchableOpacity>
+      {Array(100).fill(1).map((i, idx) => (
+        <Text key={idx}>This is a line of text; this should start scrolling eventually.</Text>
+      ))}
+    </ScrollView>
+  );
 }
 
 SettingsScreen.navigationOptions = {
